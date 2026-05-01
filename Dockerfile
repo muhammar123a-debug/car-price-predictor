5b82d2caf80df38368053f7f065ff614eee08688
Ammar <muhammar321@gmail.com>
2026-05-01 18:35:07 +0500

    add project

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# 1. Python base image
+FROM python:3.9-slim
+
+# 2. Work directory set karein
+WORKDIR /app
+
+# 3. Files copy karein
+COPY . /app
+
+# Pip ko upgrade karein taake installation smooth ho
+RUN pip install --upgrade pip
+# 4. Libraries install karein
+RUN pip install -r requirements.txt
+
+
+# 5. App run karein
+CMD ["python", "application.py"]
---
+++ application.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('LinearRegressionModel.pkl','rb'))
+df = pd.read_csv("cleaned.csv")
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    companies = sorted(df['company'].unique())
+    car_models = sorted(df['name'].unique())
+    year = sorted(df['year'].unique(), reverse=True)
+    fuel_type = sorted(df['fuel_type'].unique())
+    
+
+    prediction = ""
+    if request.method == "POST":
+        ## Get Data From The Input Form
+        company = request.form.get('company')
+        car_model =request.form.get('car_models')
+        year_of_purchase = int(request.form.get('year'))
+        fuel = request.form.get('fuel_type')
+        kms = int(request.form.get("kilo_driven"))
+
+        input_data = pd.DataFrame([[car_model, company, year_of_purchase, kms, fuel]],
+                                  columns=['name','company','year','kms_driven','fuel_type'])
+        
+        res = model.predict(input_data)[0]
+        prediction = f"Rs {np.round(res, 2)}"
+
+    return render_template('index.html', companies=companies, car_models=car_models, year=year, fuel_type=fuel_type, prediction=prediction)    
+
+if __name__ == "__main__":
+    app.run(debug=True)
